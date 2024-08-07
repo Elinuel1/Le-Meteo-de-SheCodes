f8fca7e35d5bf14fb5bfb2ad1f3cab2a3914f013
Elinuel1 <adzimanuel@gmail.com>
2024-08-07 16:25:38 +0100

    more JS work

--- index.js
+++ index.js
@@ -15,9 +15,41 @@ function refreshWeather(response) {
   let temp = document.querySelector("#temperature");
   let theTemperature = response.data.temperature.current;
   let cityType = document.querySelector("#city");
+  let descriptionType = document.querySelector("#description");
+  let humidityType = document.querySelector("#humidity");
+  let windSpeed = document.querySelector("#wind-speed");
+  let timeType = document.querySelector("#time");
+  let date = new Date(response.data.time * 1000);
 
   cityType.innerHTML = response.data.city;
+
+  humidityType.innerHTML = `${response.data.temperature.humidity}%`;
+  descriptionType.innerHTML = response.data.condition.description;
+  windSpeed.innerHTML = `${response.data.wind.speed}km/h`;
+  timeType.innerHTML = formatDate(date);
+
   temp.innerHTML = Math.round(theTemperature);
+}
+
+function formatDate(date) {
+  let minutes = date.getMinutes();
+  let hours = date.getHours();
+  let days = [
+    "Sunday",
+    "Monday",
+    "Tuesday",
+    "Wednesday",
+    "Thursday",
+    "Friday",
+    "Saturday",
+  ];
+  let day = days[date.getDay()];
+
+  if (minutes < 10) {
+    minutes = `0${minutes}`;
+  }
+
+  return `${day} ${hours}:${minutes}`;
 }
 let searchFormElement = document.querySelector("#search-form");
 searchFormElement.addEventListener("submit", handleSearchSubmit);
